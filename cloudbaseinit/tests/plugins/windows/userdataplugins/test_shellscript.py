fa5367189647169f024a91def8da61447f8b1a17
Robert Tingirica <rtingirica@cloudbasesolutions.com>
2014-03-04 15:12:28 +0200

    Updated unit tests

--- cloudbaseinit/tests/plugins/windows/userdataplugins/test_shellscript.py
+++ cloudbaseinit/tests/plugins/windows/userdataplugins/test_shellscript.py
@@ -39,34 +39,29 @@ class ShellScriptPluginTests(unittest.TestCase):
         mock_part = mock.MagicMock()
         mock_part.get_filename.return_value = filename
         mock_gettempdir.return_value = fake_dir_path
-
         mock_get_os_utils.return_value = mock_osutils
-
+        fake_target = os.path.join(fake_dir_path, filename)
         if exception:
             mock_osutils.execute_process.side_effect = [Exception]
-
         with mock.patch("cloudbaseinit.plugins.windows.userdataplugins."
                         "shellscript.open", mock.mock_open(), create=True):
             response = self._shellscript.process(mock_part)
-
         mock_part.get_filename.assert_called_once_with()
         mock_gettempdir.assert_called_once_with()
         if filename.endswith(".cmd"):
-            mock_osutils.execute_process.assert_called_with(
-                [os.path.join(fake_dir_path, filename)], True)
+            mock_osutils.execute_process.assert_called_once_with(
+                [fake_target], True)
         elif filename.endswith(".sh"):
-            mock_osutils.execute_process.assert_called_with(
-                ['bash.exe', os.path.join(fake_dir_path, filename)], False)
+            mock_osutils.execute_process.assert_called_once_with(
+                ['bash.exe', fake_target], False)
         elif filename.endswith(".py"):
-            mock_osutils.execute_process.assert_called_with(
-                ['python.exe', os.path.join(fake_dir_path, filename)], False)
+            mock_osutils.execute_process.assert_called_once_with(
+                ['python.exe', fake_target], False)
         elif filename.endswith(".ps1"):
-            mock_osutils.execute_process.assert_called_with(
-                ['powershell.exe', '-ExecutionPolicy', 'RemoteSigned',
-                 '-NonInteractive', '-File',
-                 os.path.join(fake_dir_path, filename)],
-                False)
-            self.assertFalse(response)
+            mock_osutils.execute_powershell_script.assert_called_once_with(
+                fake_target)
+        else:
+            self.assertEqual(response, 0)
 
     def test_process_cmd(self):
         self._test_process(filename='fake.cmd')
