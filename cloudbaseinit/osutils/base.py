cloudbase/cloudbase-init

commit b0aa3b56b8e6a047724adae4c57a2823ca51bca7

Fixes Python34 bugs
This fix is needed due to the different encodings on Python27 and
Python34. Some tests failed and some string values were not
compatible.

Change-Id: I7206a0549051971da76b3b30221aea9f86eed904

cloudbaseinit/osutils/base.py
@@ -19,6 +19,8 @@ import os
 import subprocess
 import sys
 
+from cloudbaseinit.utils import encoding
+
 
 class BaseOSUtils(object):
     PROTOCOL_TCP = "TCP"
@@ -34,7 +36,9 @@ class BaseOSUtils(object):
         # On Windows os.urandom() uses CryptGenRandom, which is a
         # cryptographically secure pseudorandom number generator
         b64_password = base64.b64encode(os.urandom(256))
-        return b64_password.replace('/', '').replace('+', '')[:length]
+        b64_password = encoding.get_as_string(b64_password).replace(
+            '/', '').replace('+', '')[:length]
+        return b64_password
 
     def execute_process(self, args, shell=True, decode_output=False):
         p = subprocess.Popen(args,

cloudbaseinit/metadata/services/baseopenstackservice.py
@@ -19,6 +19,7 @@ from oslo.config import cfg
 
 from cloudbaseinit.metadata.services import base
 from cloudbaseinit.openstack.common import log as logging
+from cloudbaseinit.utils import encoding
 from cloudbaseinit.utils import x509constants
 
 opts = [
@@ -48,7 +49,8 @@ class BaseOpenStackService(base.BaseMetadataService):
         path = posixpath.normpath(
             posixpath.join('openstack', version, 'meta_data.json'))
         data = self._get_cache_data(path)
-        return json.loads(data.decode('utf8'))
+        if data:
+            return json.loads(encoding.get_as_string(data))
 
     def get_instance_id(self):
         return self._get_meta_data().get('uuid')
@@ -98,10 +100,12 @@ class BaseOpenStackService(base.BaseMetadataService):
                 i += 1
 
         if not cert_data:
+
             # Look if the user_data contains a PEM certificate
             try:
                 user_data = self.get_user_data()
-                if user_data.startswith(x509constants.PEM_HEADER):
+                if user_data.startswith(
+                        x509constants.PEM_HEADER.encode()):
                     cert_data = user_data
             except base.NotExistingMetadataException:
                 LOG.debug("user_data metadata not present")

cloudbaseinit/osutils/windows.py
@@ -28,6 +28,7 @@ import wmi
 from cloudbaseinit import exception
 from cloudbaseinit.openstack.common import log as logging
 from cloudbaseinit.osutils import base
+from cloudbaseinit.utils import encoding
 from cloudbaseinit.utils.windows import network
 
 
@@ -707,9 +708,12 @@ class WindowsUtils(base.BaseOSUtils):
                 while i < forward_table.dwNumEntries:
                     row = table[i]
                     routing_table.append((
-                        Ws2_32.inet_ntoa(row.dwForwardDest),
-                        Ws2_32.inet_ntoa(row.dwForwardMask),
-                        Ws2_32.inet_ntoa(row.dwForwardNextHop),
+                        encoding.get_as_string(Ws2_32.inet_ntoa(
+                            row.dwForwardDest)),
+                        encoding.get_as_string(Ws2_32.inet_ntoa(
+                            row.dwForwardMask)),
+                        encoding.get_as_string(Ws2_32.inet_ntoa(
+                            row.dwForwardNextHop)),
                         row.dwForwardIfIndex,
                         row.dwForwardMetric1))
                     i += 1

cloudbaseinit/plugins/windows/networkconfig.py
@@ -22,6 +22,7 @@ from cloudbaseinit.metadata.services import base as service_base
 from cloudbaseinit.openstack.common import log as logging
 from cloudbaseinit.osutils import factory as osutils_factory
 from cloudbaseinit.plugins import base as plugin_base
+from cloudbaseinit.utils import encoding
 
 
 LOG = logging.getLogger(__name__)
@@ -58,6 +59,7 @@ class NetworkConfigPlugin(plugin_base.BasePlugin):
             content_path = network_config['content_path']
             content_name = content_path.rsplit('/', 1)[-1]
             debian_network_conf = service.get_content(content_name)
+            debian_network_conf = encoding.get_as_string(debian_network_conf)
 
             LOG.debug('network config content:\n%s' % debian_network_conf)
 

cloudbaseinit/plugins/windows/setuserpassword.py
@@ -47,7 +47,7 @@ class SetUserPasswordPlugin(base.BasePlugin):
     def _get_ssh_public_key(self, service):
         public_keys = service.get_public_keys()
         if public_keys:
-            return public_keys[0]
+            return list(public_keys)[0]
 
     def _get_password(self, service, osutils):
         if CONF.inject_user_password:

cloudbaseinit/plugins/windows/userdata.py
@@ -23,6 +23,7 @@ from cloudbaseinit.openstack.common import log as logging
 from cloudbaseinit.plugins import base
 from cloudbaseinit.plugins.windows.userdataplugins import factory
 from cloudbaseinit.plugins.windows import userdatautils
+from cloudbaseinit.utils import encoding
 
 LOG = logging.getLogger(__name__)
 
@@ -40,6 +41,7 @@ class UserDataPlugin(base.BasePlugin):
         if not user_data:
             return (base.PLUGIN_EXECUTION_DONE, False)
 
+        LOG.debug('User data content length: %d' % len(user_data))
         user_data = self._check_gzip_compression(user_data)
 
         return self._process_user_data(user_data)
@@ -53,14 +55,16 @@ class UserDataPlugin(base.BasePlugin):
         return user_data
 
     def _parse_mime(self, user_data):
-        return email.message_from_string(user_data).walk()
+        user_data_str = encoding.get_as_string(user_data)
+        LOG.debug('User data content:\n%s' % user_data_str)
+
+        return email.message_from_string(user_data_str).walk()
 
     def _process_user_data(self, user_data):
         plugin_status = base.PLUGIN_EXECUTION_DONE
         reboot = False
 
-        LOG.debug('User data content:\n%s' % user_data)
-        if user_data.startswith('Content-Type: multipart'):
+        if user_data.startswith(b'Content-Type: multipart'):
             user_data_plugins = factory.load_plugins()
             user_handlers = {}
 
@@ -158,7 +162,7 @@ class UserDataPlugin(base.BasePlugin):
         return (plugin_status, reboot)
 
     def _process_non_multi_part(self, user_data):
-        if user_data.startswith('#cloud-config'):
+        if user_data.startswith(b'#cloud-config'):
             user_data_plugins = factory.load_plugins()
             cloud_config_plugin = user_data_plugins.get('text/cloud-config')
             ret_val = cloud_config_plugin.process(user_data)

cloudbaseinit/plugins/windows/userdataplugins/heat.py
@@ -20,6 +20,7 @@ from oslo.config import cfg
 from cloudbaseinit.openstack.common import log as logging
 from cloudbaseinit.plugins.windows.userdataplugins import base
 from cloudbaseinit.plugins.windows import userdatautils
+from cloudbaseinit.utils import encoding
 
 opts = [
     cfg.StrOpt('heat_config_dir', default='C:\\cfn', help='The directory '
@@ -47,8 +48,7 @@ class HeatPlugin(base.BaseUserDataPlugin):
         file_name = os.path.join(CONF.heat_config_dir, part.get_filename())
         self._check_dir(file_name)
 
-        with open(file_name, 'wb') as f:
-            f.write(part.get_payload())
+        encoding.write_file(file_name, part.get_payload())
 
         if part.get_filename() == self._heat_user_data_filename:
             return userdatautils.execute_user_data_script(part.get_payload())

cloudbaseinit/plugins/windows/userdataplugins/shellscript.py
@@ -19,6 +19,7 @@ import tempfile
 from cloudbaseinit.openstack.common import log as logging
 from cloudbaseinit.plugins.windows import fileexecutils
 from cloudbaseinit.plugins.windows.userdataplugins import base
+from cloudbaseinit.utils import encoding
 
 LOG = logging.getLogger(__name__)
 
@@ -32,8 +33,7 @@ class ShellScriptPlugin(base.BaseUserDataPlugin):
         target_path = os.path.join(tempfile.gettempdir(), file_name)
 
         try:
-            with open(target_path, 'wb') as f:
-                f.write(part.get_payload())
+            encoding.write_file(target_path, part.get_payload())
 
             return fileexecutils.exec_file(target_path)
         except Exception as ex:

commit 3758c1a3b2112842f4ff702a053733d7d912c48a

Fixes encrypted random password length issue
An issue related to unicode / bytes conversion has been
introduced by commit b0aa3b56b8e6a047724adae4c57a2823ca51bca7.
This patch fixes the issue by reverting to returning bytes
instead of unicode.

Change-Id: Ifab486692eea008a46880ee4229e68f9401a48f9
Closes-Bug: #1407758

cloudbaseinit/osutils/base.py
@@ -19,8 +19,6 @@ import os
 import subprocess
 import sys
 
-from cloudbaseinit.utils import encoding
-
 
 class BaseOSUtils(object):
     PROTOCOL_TCP = "TCP"
@@ -36,9 +34,7 @@ class BaseOSUtils(object):
         # On Windows os.urandom() uses CryptGenRandom, which is a
         # cryptographically secure pseudorandom number generator
         b64_password = base64.b64encode(os.urandom(256))
-        b64_password = encoding.get_as_string(b64_password).replace(
-            '/', '').replace('+', '')[:length]
-        return b64_password
+        return b64_password.replace(b'/', b'').replace(b'+', b'')[:length]
 
     def execute_process(self, args, shell=True, decode_output=False):
         p = subprocess.Popen(args,