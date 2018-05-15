df0e5c242b938f3df9d6f74c1a5eda15f779d20b
Dan Feon <dolr@users.noreply.github.com>
2018-05-14 17:00:54 -0700

    add support to foreman_userdata for Windows build on Foreman

---
+++ cloudbaseinit/conf/foreman.py
@@ -0,0 +1,52 @@
+# Copyright 2016 Cloudbase Solutions Srl
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Config options available for the foreman metadata service."""
+
+from oslo_config import cfg
+
+from cloudbaseinit.conf import base as conf_base
+
+
+class ForemanOptions(conf_base.Options):
+
+    """Config options available for the foreman metadata service."""
+
+    def __init__(self, config):
+        super(ForemanOptions, self).__init__(config, group="foreman")
+        self._options = [
+            cfg.StrOpt(
+                "metadata_base_url", default="http://foreman.example.invalid/",
+                help="The base URL where the service looks for metadata",
+                deprecated_name="foreman_metadata_base_url",
+                deprecated_group="DEFAULT"),
+            cfg.BoolOpt(
+                "https_allow_insecure", default=False,
+                help="Whether to disable the validation of HTTPS "
+                     "certificates."),
+            cfg.StrOpt(
+                "https_ca_bundle", default=None,
+                help="The path to a CA_BUNDLE file or directory with "
+                     "certificates of trusted CAs."),
+        ]
+
+    def register(self):
+        """Register the current options to the global ConfigOpts object."""
+        group = cfg.OptGroup(self.group_name, title='Foreman Options')
+        self._config.register_group(group)
+        self._config.register_opts(self._options, group=group)
+
+    def list(self):
+        """Return a list which contains all the available options."""
+        return self._options
