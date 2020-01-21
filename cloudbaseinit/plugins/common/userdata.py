rhockenbury/cloudbase-init-1

Add support for jinja as userdata template engine
If the userdata starts with the text '## template:jinja',
Jinja2 templating engine will be used to render that
userdata.

This feature leverages the get_instance_data metadata
service implementation and tries to be as close as possible
with the cloud-init's implementation.

Change-Id: I79cb0066f7d65ae27867ac01b443e9be432b4dc3

cloudbaseinit/plugins/common/userdata.py
@@ -26,6 +26,7 @@ from cloudbaseinit.plugins.common import execcmd
 from cloudbaseinit.plugins.common.userdataplugins import factory
 from cloudbaseinit.plugins.common import userdatautils
 from cloudbaseinit.utils import encoding
+from cloudbaseinit.utils.template_engine import factory as template_factory
 from cloudbaseinit.utils import x509constants
 
 
@@ -53,7 +54,7 @@ class UserDataPlugin(base.BasePlugin):
             self._write_userdata(user_data, user_data_path)
 
         if CONF.process_userdata:
-            return self._process_user_data(user_data)
+            return self._process_user_data(user_data, service)
         return base.PLUGIN_EXECUTION_DONE, False
 
     @staticmethod
@@ -100,7 +101,7 @@ class UserDataPlugin(base.BasePlugin):
                                                    "The user data content is "
                                                    "either invalid or empty.")
 
-    def _process_user_data(self, user_data):
+    def _process_user_data(self, user_data, service):
         plugin_status = base.PLUGIN_EXECUTION_DONE
         reboot = False
         headers = self._get_headers(user_data)
@@ -122,7 +123,7 @@ class UserDataPlugin(base.BasePlugin):
 
             return plugin_status, reboot
         else:
-            return self._process_non_multi_part(user_data)
+            return self._process_non_multi_part(user_data, service)
 
     def _process_part(self, part, user_data_plugins, user_handlers):
         ret_val = None
@@ -186,8 +187,16 @@ class UserDataPlugin(base.BasePlugin):
         LOG.debug("Calling part handler \"__end__\" event")
         handler_func(None, "__end__", None, None)
 
-    def _process_non_multi_part(self, user_data):
+    def _process_non_multi_part(self, user_data, service):
         ret_val = None
+
+        template_engine = template_factory.get_template_manager(user_data)
+        if template_engine:
+            user_data = template_engine.render(
+                service.get_instance_data(),
+                user_data
+            )
+
         if user_data.startswith(b'#cloud-config'):
             user_data_plugins = factory.load_plugins()
             cloud_config_plugin = user_data_plugins.get('text/cloud-config')

cloudbase/cloudbase-init

cloudbaseinit/plugins/common/userdata.py
@@ -26,6 +26,7 @@ from cloudbaseinit.plugins.common import execcmd
 from cloudbaseinit.plugins.common.userdataplugins import factory
 from cloudbaseinit.plugins.common import userdatautils
 from cloudbaseinit.utils import encoding
+from cloudbaseinit.utils.template_engine import factory as template_factory
 from cloudbaseinit.utils import x509constants
 
 
@@ -53,7 +54,7 @@ class UserDataPlugin(base.BasePlugin):
             self._write_userdata(user_data, user_data_path)
 
         if CONF.process_userdata:
-            return self._process_user_data(user_data)
+            return self._process_user_data(user_data, service)
         return base.PLUGIN_EXECUTION_DONE, False
 
     @staticmethod
@@ -100,7 +101,7 @@ class UserDataPlugin(base.BasePlugin):
                                                    "The user data content is "
                                                    "either invalid or empty.")
 
-    def _process_user_data(self, user_data):
+    def _process_user_data(self, user_data, service):
         plugin_status = base.PLUGIN_EXECUTION_DONE
         reboot = False
         headers = self._get_headers(user_data)
@@ -122,7 +123,7 @@ class UserDataPlugin(base.BasePlugin):
 
             return plugin_status, reboot
         else:
-            return self._process_non_multi_part(user_data)
+            return self._process_non_multi_part(user_data, service)
 
     def _process_part(self, part, user_data_plugins, user_handlers):
         ret_val = None
@@ -186,8 +187,16 @@ class UserDataPlugin(base.BasePlugin):
         LOG.debug("Calling part handler \"__end__\" event")
         handler_func(None, "__end__", None, None)
 
-    def _process_non_multi_part(self, user_data):
+    def _process_non_multi_part(self, user_data, service):
         ret_val = None
+
+        template_engine = template_factory.get_template_engine(user_data)
+        if template_engine:
+            user_data = template_engine.render(
+                service.get_instance_data(),
+                user_data
+            )
+
         if user_data.startswith(b'#cloud-config'):
             user_data_plugins = factory.load_plugins()
             cloud_config_plugin = user_data_plugins.get('text/cloud-config')