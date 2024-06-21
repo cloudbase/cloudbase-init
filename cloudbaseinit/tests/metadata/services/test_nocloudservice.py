cloudbase/cloudbase-init

commit f0fe66b30bf95d05ebecb74d8059ea2cdda37d0f

Add network config support to VMware guest info service
This commit adds the network config support for VMware environment.
Both NetworkConfig V1 and V2 formats are supported to align with
cloud-init.

NetworkConfig V2 format specification can be found at
https://cloudinit.readthedocs.io/en/latest/reference/network-config-format-v2.html

Change-Id: I6c686a88be253858e9f46599f10dca571a76b85e
Signed-off-by: Zhongcheng Lao <zhongchengl@vmware.com>

cloudbaseinit/metadata/services/nocloudservice.py
@@ -11,6 +11,8 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
+import copy
 import netaddr
 
 from oslo_log import log as oslo_logging
@@ -235,6 +237,10 @@ class NoCloudNetworkConfigV1Parser(object):
         networks = []
         services = []
 
+        network_config = network_config.get('network') \
+            if network_config else {}
+        network_config = network_config.get('config') \
+            if network_config else None
         if not network_config:
             LOG.warning("Network configuration is empty")
             return
@@ -270,6 +276,265 @@ class NoCloudNetworkConfigV1Parser(object):
             networks=networks,
             services=services
         )
+
+
+class NoCloudNetworkConfigV2Parser(object):
+    DEFAULT_GATEWAY_CIDR_IPV4 = u"0.0.0.0/0"
+    DEFAULT_GATEWAY_CIDR_IPV6 = u"::/0"
+
+    NETWORK_LINK_TYPE_ETHERNET = 'ethernet'
+    NETWORK_LINK_TYPE_BOND = 'bond'
+    NETWORK_LINK_TYPE_VLAN = 'vlan'
+    NETWORK_LINK_TYPE_BRIDGE = 'bridge'
+
+    SUPPORTED_NETWORK_CONFIG_TYPES = {
+        NETWORK_LINK_TYPE_ETHERNET: 'ethernets',
+        NETWORK_LINK_TYPE_BOND: 'bonds',
+        NETWORK_LINK_TYPE_VLAN: 'vlans',
+    }
+
+    def _parse_mac_address(self, item):
+        return item.get("match", {}).get("macaddress")
+
+    def _parse_addresses(self, item, link_name):
+        networks = []
+        services = []
+
+        routes = []
+        # handle route config in deprecated gateway4/gateway6
+        gateway4 = item.get("gateway4")
+        gateway6 = item.get("gateway6")
+        default_route = None
+        if gateway6 and netaddr.valid_ipv6(gateway6):
+            default_route = network_model.Route(
+                network_cidr=self.DEFAULT_GATEWAY_CIDR_IPV6,
+                gateway=gateway6)
+        elif gateway4 and netaddr.valid_ipv4(gateway4):
+            default_route = network_model.Route(
+                network_cidr=self.DEFAULT_GATEWAY_CIDR_IPV4,
+                gateway=gateway4)
+        if default_route:
+            routes.append(default_route)
+
+        # netplan format config
+        routes_config = item.get("routes", {})
+        for route_config in routes_config:
+            network_cidr = route_config.get("to")
+            gateway = route_config.get("via")
+            if network_cidr.lower() == "default":
+                if netaddr.valid_ipv6(gateway):
+                    network_cidr = self.DEFAULT_GATEWAY_CIDR_IPV6
+                else:
+                    network_cidr = self.DEFAULT_GATEWAY_CIDR_IPV4
+            route = network_model.Route(
+                network_cidr=network_cidr,
+                gateway=gateway)
+            routes.append(route)
+
+        nameservers = item.get("nameservers")
+        nameserver_addresses = nameservers.get("addresses", []) \
+            if nameservers else []
+        searches = nameservers.get("search", [])
+        service = network_model.NameServerService(
+            addresses=nameserver_addresses,
+            search=','.join(searches) if searches else None,
+        )
+        services.append(service)
+
+        addresses = item.get("addresses", [])
+        for addr in addresses:
+            network = network_model.Network(
+                link=link_name,
+                address_cidr=addr,
+                dns_nameservers=nameserver_addresses,
+                routes=routes
+            )
+            networks.append(network)
+
+        return networks, services
+
+    def _parse_ethernet_config_item(self, item):
+        if not item.get('name'):
+            LOG.warning("Ethernet does not have a name.")
+            return
+
+        name = item.get('name')
+        eth_name = item.get("set-name", name)
+        link = network_model.Link(
+            id=name,
+            name=eth_name,
+            type=network_model.LINK_TYPE_PHYSICAL,
+            enabled=True,
+            mac_address=self._parse_mac_address(item),
+            mtu=item.get('mtu'),
+            bond=None,
+            vlan_link=None,
+            vlan_id=None
+        )
+
+        networks, services = self._parse_addresses(item, link.name)
+        return network_model.NetworkDetailsV2(
+            links=[link],
+            networks=networks,
+            services=services,
+        )
+
+    def _parse_bond_config_item(self, item):
+        if not item.get('name'):
+            LOG.warning("Bond does not have a name.")
+            return
+
+        bond_params = item.get('parameters')
+        if not bond_params:
+            LOG.warning("Bond does not have parameters")
+            return
+
+        bond_mode = bond_params.get('mode')
+        if bond_mode not in network_model.AVAILABLE_BOND_TYPES:
+            raise exception.CloudbaseInitException(
+                "Unsupported bond mode: %s" % bond_mode)
+
+        bond_lacp_rate = None
+        if bond_mode == network_model.BOND_TYPE_8023AD:
+            bond_lacp_rate = bond_params.get('lacp-rate')
+            if (bond_lacp_rate and bond_lacp_rate not in
+                    network_model.AVAILABLE_BOND_LACP_RATES):
+                raise exception.CloudbaseInitException(
+                    "Unsupported bond lacp rate: %s" % bond_lacp_rate)
+
+        bond_xmit_hash_policy = bond_params.get('transmit-hash-policy')
+        if (bond_xmit_hash_policy and bond_xmit_hash_policy not in
+                network_model.AVAILABLE_BOND_LB_ALGORITHMS):
+            raise exception.CloudbaseInitException(
+                "Unsupported bond hash policy: %s" %
+                bond_xmit_hash_policy)
+
+        bond_interfaces = item.get('interfaces')
+
+        bond = network_model.Bond(
+            members=bond_interfaces,
+            type=bond_mode,
+            lb_algorithm=bond_xmit_hash_policy,
+            lacp_rate=bond_lacp_rate,
+        )
+
+        link = network_model.Link(
+            id=item.get('name'),
+            name=item.get('name'),
+            type=network_model.LINK_TYPE_BOND,
+            enabled=True,
+            mac_address=self._parse_mac_address(item),
+            mtu=item.get('mtu'),
+            bond=bond,
+            vlan_link=None,
+            vlan_id=None
+        )
+
+        networks, services = self._parse_addresses(item, link.name)
+        return network_model.NetworkDetailsV2(
+            links=[link],
+            networks=networks,
+            services=services
+        )
+
+    def _parse_vlan_config_item(self, item):
+        if not item.get('name'):
+            LOG.warning("VLAN NIC does not have a name.")
+            return
+
+        link = network_model.Link(
+            id=item.get('name'),
+            name=item.get('name'),
+            type=network_model.LINK_TYPE_VLAN,
+            enabled=True,
+            mac_address=self._parse_mac_address(item),
+            mtu=item.get('mtu'),
+            bond=None,
+            vlan_link=item.get('link'),
+            vlan_id=item.get('id')
+        )
+
+        networks, services = self._parse_addresses(item, link.name)
+        return network_model.NetworkDetailsV2(
+            links=[link],
+            networks=networks,
+            services=services,
+        )
+
+    def _get_network_config_parser(self, parser_type):
+        parsers = {
+            self.NETWORK_LINK_TYPE_ETHERNET: self._parse_ethernet_config_item,
+            self.NETWORK_LINK_TYPE_BOND: self._parse_bond_config_item,
+            self.NETWORK_LINK_TYPE_VLAN: self._parse_vlan_config_item,
+        }
+        parser = parsers.get(parser_type)
+        if not parser:
+            raise exception.CloudbaseInitException(
+                "Network config parser '%s' does not exist",
+                parser_type)
+        return parser
+
+    def parse(self, network_config):
+        links = []
+        networks = []
+        services = []
+
+        network_config = network_config.get('network') \
+            if network_config else {}
+        if not network_config:
+            LOG.warning("Network configuration is empty")
+            return
+
+        if not isinstance(network_config, dict):
+            LOG.warning("Network config '%s' is not a dict.",
+                        network_config)
+            return
+
+        for singular, plural in self.SUPPORTED_NETWORK_CONFIG_TYPES.items():
+            network_config_items = network_config.get(plural, {})
+            if not network_config_items:
+                continue
+
+            if not isinstance(network_config_items, dict):
+                LOG.warning("Network config '%s' is not a dict",
+                            network_config_items)
+                continue
+
+            for name, network_config_item in network_config_items.items():
+                if not isinstance(network_config_item, dict):
+                    LOG.warning(
+                        "network config item '%s' of type %s is not a dict",
+                        network_config_item, singular)
+                    continue
+
+                item = copy.deepcopy(network_config_item)
+                item['name'] = name
+                net_details = (
+                    self._get_network_config_parser(singular)
+                    (item))
+
+                if net_details:
+                    links += net_details.links
+                    networks += net_details.networks
+                    services += net_details.services
+
+        return network_model.NetworkDetailsV2(
+            links=links,
+            networks=networks,
+            services=services
+        )
+
+
+class NoCloudNetworkConfigParser(object):
+
+    @staticmethod
+    def parse(network_data):
+        network_data_version = network_data.get("network", {}).get("version")
+        if network_data_version == 1:
+            network_config_parser = NoCloudNetworkConfigV1Parser()
+            return network_config_parser.parse(network_data)
+
+        return NoCloudNetworkConfigV2Parser().parse(network_data)
 
 
 class NoCloudConfigDriveService(baseconfigdrive.BaseConfigDriveService):
@@ -334,11 +599,4 @@ class NoCloudConfigDriveService(baseconfigdrive.BaseConfigDriveService):
             LOG.exception("V2 network metadata could not be deserialized")
             return
 
-        network_data_version = network_data.get("version")
-        if network_data_version != 1:
-            LOG.error("Network data version '%s' is not supported",
-                      network_data_version)
-            return
-
-        network_config_parser = NoCloudNetworkConfigV1Parser()
-        return network_config_parser.parse(network_data.get("config"))
+        return NoCloudNetworkConfigParser.parse(network_data)

commit 1a98b6742a623ce2da99525eae5e244d7f311af6

nocloud: fix network config regression
With commit
https://github.com/cloudbase/cloudbase-init/commit/f0fe66b30bf95d05ebecb74d8059ea2cdda37d0f,
the network config in case of NoCloudConfigDrive was supported only if
it was in the format:

```yaml
network:
  config: ...
  version: 1|2
```

Before the commit, the following format was also supported:

```yaml
config: ...
version: 1
```

Added support for both formats and made sure that the `network` key is
not required.

Change-Id: I7478a8654443db83f0f3995839811910c468a06c
Signed-off-by: Adrian Vladu <avladu@cloudbasesolutions.com>

cloudbaseinit/metadata/services/nocloudservice.py
@@ -237,10 +237,11 @@ class NoCloudNetworkConfigV1Parser(object):
         networks = []
         services = []
 
-        network_config = network_config.get('network') \
-            if network_config else {}
-        network_config = network_config.get('config') \
-            if network_config else None
+        if network_config and network_config.get('network'):
+            network_config = network_config.get('network')
+        if network_config:
+            network_config = network_config.get('config')
+
         if not network_config:
             LOG.warning("Network configuration is empty")
             return
@@ -479,8 +480,9 @@ class NoCloudNetworkConfigV2Parser(object):
         networks = []
         services = []
 
-        network_config = network_config.get('network') \
-            if network_config else {}
+        if network_config and network_config.get('network'):
+            network_config = network_config.get('network')
+
         if not network_config:
             LOG.warning("Network configuration is empty")
             return
@@ -529,12 +531,21 @@ class NoCloudNetworkConfigParser(object):
 
     @staticmethod
     def parse(network_data):
-        network_data_version = network_data.get("network", {}).get("version")
+        # we can have a network key in some cases
+        if network_data.get("network"):
+            network_data = network_data.get("network")
+        network_data_version = network_data.get("version")
+
         if network_data_version == 1:
             network_config_parser = NoCloudNetworkConfigV1Parser()
-            return network_config_parser.parse(network_data)
+        elif network_data_version == 2:
+            network_config_parser = NoCloudNetworkConfigV2Parser()
+        else:
+            raise exception.CloudbaseInitException(
+                "Unsupported network_data_version: '%s'"
+                % network_data_version)
 
-        return NoCloudNetworkConfigV2Parser().parse(network_data)
+        return network_config_parser.parse(network_data)
 
 
 class NoCloudConfigDriveService(baseconfigdrive.BaseConfigDriveService):

cloudbaseinit/tests/metadata/services/test_nocloudservice.py
@@ -15,6 +15,7 @@
 import ddt
 import importlib
 import os
+import textwrap
 import unittest
 import unittest.mock as mock
 
@@ -48,55 +49,57 @@ network:
   config:
   - type: router
 """
-NOCLOUD_NETWORK_CONFIG_TEST_DATA_V1 = """
-network:
-  version: 1
-  config:
-  - type: physical
-    name: interface0
-    mac_address: "52:54:00:12:34:00"
-    mtu: 1450
-    subnets:
-    - type: static
-      address: 192.168.1.10
-      netmask: 255.255.255.0
-      gateway: 192.168.1.1
-      dns_nameservers:
-      - 192.168.1.11
-  - type: bond
-    name: bond0
-    bond_interfaces:
-    - gbe0
-    - gbe1
-    mac_address: "52:54:00:12:34:00"
-    params:
-      bond-mode: active-backup
-      bond-lacp-rate: false
-    mtu: 1450
-    subnets:
-    - type: static
-      address: 192.168.1.10
-      netmask: 255.255.255.0
-      dns_nameservers:
-      - 192.168.1.11
-  - type: vlan
-    name: vlan0
-    vlan_link: eth1
-    vlan_id: 150
-    mac_address: "52:54:00:12:34:00"
-    mtu: 1450
-    subnets:
-    - type: static
-      address: 192.168.1.10
-      netmask: 255.255.255.0
-      dns_nameservers:
-      - 192.168.1.11
-  - type: nameserver
-    address:
-    - 192.168.23.2
-    - 8.8.8.8
-    search: acme.local
+NOCLOUD_NETWORK_CONFIG_TEST_DATA_V1_LEGACY = """
+version: 1
+config:
+- type: physical
+  name: interface0
+  mac_address: "52:54:00:12:34:00"
+  mtu: 1450
+  subnets:
+  - type: static
+    address: 192.168.1.10
+    netmask: 255.255.255.0
+    gateway: 192.168.1.1
+    dns_nameservers:
+    - 192.168.1.11
+- type: bond
+  name: bond0
+  bond_interfaces:
+  - gbe0
+  - gbe1
+  mac_address: "52:54:00:12:34:00"
+  params:
+    bond-mode: active-backup
+    bond-lacp-rate: false
+  mtu: 1450
+  subnets:
+  - type: static
+    address: 192.168.1.10
+    netmask: 255.255.255.0
+    dns_nameservers:
+    - 192.168.1.11
+- type: vlan
+  name: vlan0
+  vlan_link: eth1
+  vlan_id: 150
+  mac_address: "52:54:00:12:34:00"
+  mtu: 1450
+  subnets:
+  - type: static
+    address: 192.168.1.10
+    netmask: 255.255.255.0
+    dns_nameservers:
+    - 192.168.1.11
+- type: nameserver
+  address:
+  - 192.168.23.2
+  - 8.8.8.8
+  search: acme.local
 """
+NOCLOUD_NETWORK_CONFIG_TEST_DATA_V1 = """
+network:%s
+""" % (textwrap.indent(NOCLOUD_NETWORK_CONFIG_TEST_DATA_V1_LEGACY, "  "))
 NOCLOUD_NETWORK_CONFIG_TEST_DATA_V2_EMPTY_CONFIG = """
 """
 NOCLOUD_NETWORK_CONFIG_TEST_DATA_V2_CONFIG_IS_NOT_DICT = """
@@ -116,67 +119,69 @@ network:
     eth0:
      - test
 """
-NOCLOUD_NETWORK_CONFIG_TEST_DATA_V2 = """
-network:
-  version: 2
-  ethernets:
-    interface0:
-      match:
-        macaddress: "52:54:00:12:34:00"
-      set-name: "eth0"
+NOCLOUD_NETWORK_CONFIG_TEST_DATA_V2_LEGACY = """
+version: 2
+ethernets:
+  interface0:
+    match:
+      macaddress: "52:54:00:12:34:00"
+    set-name: "eth0"
+    addresses:
+    - 192.168.1.10/24
+    gateway4: 192.168.1.1
+    nameservers:
       addresses:
-      - 192.168.1.10/24
-      gateway4: 192.168.1.1
-      nameservers:
-        addresses:
-        - 192.168.1.11
-        - 192.168.1.12
-        search:
-        - acme.local
-      mtu: 1450
-    interface1:
-      set-name: "interface1"
+      - 192.168.1.11
+      - 192.168.1.12
+      search:
+      - acme.local
+    mtu: 1450
+  interface1:
+    set-name: "interface1"
+    addresses:
+    - 192.168.1.100/24
+    gateway4: 192.168.1.1
+    nameservers:
       addresses:
-      - 192.168.1.100/24
-      gateway4: 192.168.1.1
-      nameservers:
-        addresses:
-        - 192.168.1.11
-        - 192.168.1.12
-        search:
-        - acme.local
-  bonds:
-    bond0:
-      interfaces: ["gbe0", "gbe1"]
-      match:
-        macaddress: "52:54:00:12:34:00"
-      parameters:
-        mode: active-backup
-        lacp-rate: false
+      - 192.168.1.11
+      - 192.168.1.12
+      search:
+      - acme.local
+bonds:
+  bond0:
+    interfaces: ["gbe0", "gbe1"]
+    match:
+      macaddress: "52:54:00:12:34:00"
+    parameters:
+      mode: active-backup
+      lacp-rate: false
+    addresses:
+    - 192.168.1.10/24
+    nameservers:
       addresses:
-      - 192.168.1.10/24
-      nameservers:
-        addresses:
-        - 192.168.1.11
-      mtu: 1450
-  vlans:
-    vlan0:
-      id: 150
-      link: eth1
-      dhcp4: yes
-      match:
-        macaddress: "52:54:00:12:34:00"
+      - 192.168.1.11
+    mtu: 1450
+vlans:
+  vlan0:
+    id: 150
+    link: eth1
+    dhcp4: yes
+    match:
+      macaddress: "52:54:00:12:34:00"
+    addresses:
+    - 192.168.1.10/24
+    nameservers:
       addresses:
-      - 192.168.1.10/24
-      nameservers:
-        addresses:
-        - 192.168.1.11
-      mtu: 1450
-  bridges:
-    br0:
-      interfaces: ['eth0']
-      dhcp4: true
+      - 192.168.1.11
+    mtu: 1450
+bridges:
+  br0:
+    interfaces: ['eth0']
+    dhcp4: true
 """
+NOCLOUD_NETWORK_CONFIG_TEST_DATA_V2 = """
+network:%s
+""" % (textwrap.indent(NOCLOUD_NETWORK_CONFIG_TEST_DATA_V2_LEGACY, "  "))
 
 
 @ddt.ddt
@@ -207,7 +212,12 @@ class TestNoCloudNetworkConfigV1Parser(unittest.TestCase):
         self.assertEqual(True, expected_result[0] in self.snatcher.output[0])
         self.assertEqual(result, expected_result[1])
 
-    def test_network_details_v2(self):
+    @ddt.data(
+        (NOCLOUD_NETWORK_CONFIG_TEST_DATA_V1, True),
+        (NOCLOUD_NETWORK_CONFIG_TEST_DATA_V1_LEGACY, True)
+    )
+    @ddt.unpack
+    def test_network_details_v2(self, test_data, expected_result):
         expected_bond = nm.Bond(
             members=["gbe0", "gbe1"],
             type=nm.BOND_TYPE_ACTIVE_BACKUP,
@@ -275,7 +285,7 @@ class TestNoCloudNetworkConfigV1Parser(unittest.TestCase):
             search='acme.local')
 
         result = self._parser.parse(
-            serialization.parse_json_yaml(NOCLOUD_NETWORK_CONFIG_TEST_DATA_V1))
+            serialization.parse_json_yaml(test_data))
 
         self.assertEqual(result.links[0], expected_link)
         self.assertEqual(result.networks[0], expected_network)
@@ -318,7 +328,12 @@ class TestNoCloudNetworkConfigV2Parser(unittest.TestCase):
         self.assertEqual(True, expected_result[0] in self.snatcher.output[0])
         self.assertEqual(result, expected_result[1])
 
-    def test_network_details_v2(self):
+    @ddt.data(
+        (NOCLOUD_NETWORK_CONFIG_TEST_DATA_V2, True),
+        (NOCLOUD_NETWORK_CONFIG_TEST_DATA_V2_LEGACY, True)
+    )
+    @ddt.unpack
+    def test_network_details_v2(self, test_data, expected_result):
         expected_bond = nm.Bond(
             members=["gbe0", "gbe1"],
             type=nm.BOND_TYPE_ACTIVE_BACKUP,
@@ -406,7 +421,7 @@ class TestNoCloudNetworkConfigV2Parser(unittest.TestCase):
             search='acme.local')
 
         result = self._parser.parse(
-            serialization.parse_json_yaml(NOCLOUD_NETWORK_CONFIG_TEST_DATA_V2))
+            serialization.parse_json_yaml(test_data))
 
         self.assertEqual(result.links[0], expected_link)
         self.assertEqual(result.links[1], expected_link_if1)